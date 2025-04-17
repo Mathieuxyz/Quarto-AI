0ce4c7928264ab8c94991331c0ecb1680a089129
Mathieu Franchimont <franchimont.mathieu@gmail.com>
2025-04-17 11:41:08 +0200

    Connection to the server established

--- Socket/server_connector.py
+++ Socket/server_connector.py
@@ -7,29 +7,38 @@ class Client:
         self.host = host
         self.port = port
 
-    def connection(self):
-        while True:
-            # Creates a socket
-            self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.client_socket.connect((self.host, self.port))
+    def connect(self, message: dict):
+        
+        try: # Connect to the server
+            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.s.connect((self.host, self.port))
+            print('Client connected')  
+            # Send the message to the server
+            self.s.send(json.dumps(message).encode())
+            print('Connection request sent')  
 
-            response = json.loads(self.client_socket.recv(2048).decode())
+            while True:
 
-            if response == 'ping':
-                print('ping')
-                self.client_socket.send(json.dumps({"response": "pong"}))
+                try:
+                    response = (self.s.recv(4000).decode())
+                    response = json.loads(response)
 
-    def send_message(self, message: dict)->None:
-        # Connect to the server
-        self.connection()  
-        # Send the message to the server
-        self.client_socket.send(json.dumps(message).encode())  
+                    if response["response"] == "ping":
+                        self.s.send(json.dumps({"response": "pong"}))
+                        print('pong')
+                    else:
+                        print(f'Server response {response}')
+                except:
+                    pass
+        except ConnectionRefusedError as e:
+            print(f'Connection failed: {e}')
+            
 
 client1 = Client('127.0.0.1', 3000)
 message = {"request": "subscribe",
-           "port": 3000,
+           "port": 4000,
            "name": "Madame_Henrotte_gaming",
            "matricules": ["23363", "23049"]
            }
-client1.send_message(message)
+client1.connect(message)
         
