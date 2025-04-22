34631fbcffb72a6455005499408446ae12ad0703
Mastrogiovanni Amedeo <23046@ecam.be>
2025-04-22 10:04:14 +0200

    chose random case

--- functions/data_collection.py
+++ functions/data_collection.py
@@ -62,4 +62,12 @@ def give_piece():
     choices = pieces - played
     return random.choice(list(choices))
 
+def chose_case():
+    free = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15}
+
+    for i in board:
+        free = free - {i}
+    return random.choice(list(free))
+
+print(chose_case())
 print(give_piece())
