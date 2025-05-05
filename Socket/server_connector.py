e595ea8e0a064a4f0eab77de17ec0e13fffb3119
Mathieu Franchimont <franchimont.mathieu@gmail.com>
2025-05-05 09:51:41 +0200

    The client can fully connect, receive and send requests

--- Socket/server_connector.py
+++ Socket/server_connector.py
@@ -26,7 +26,7 @@ class Client: #We will first connect the client to the server to subscribe himse
 
             # Send the message to the server
 
-            self.message_sender(self.message)
+            self.s.send(json.dumps(self.message).encode())
 
             self.s.close() 
 
@@ -51,18 +51,19 @@ class Client: #We will first connect the client to the server to subscribe himse
 
             self.s.listen()
 
-            self.client, self.adress = self.s.accept()
-
-
             while True: #Once the socket is connected, this While True makes sures all the messages are received
 
                 try:
+
+                    self.client, self.adress = self.s.accept()
                     
                     response = self.client.recv(1024)
 
                     response = json.loads(response)
 
                     self.response_treatment(response)
+
+                    self.client.close()
 
                 except:
 
@@ -75,11 +76,14 @@ class Client: #We will first connect the client to the server to subscribe himse
 
     def response_treatment(self, response): #This is like a centre were all received messages from the server are managed
 
-        if response["response"] == "ping": #ping pong response
+        print(response)
+
+        if response["request"] == "ping": #ping pong response
 
             self.message_sender({"response": "pong"})
+            print('pong')
 
-        elif response["response"] == "play": #play response
+        elif response["request"] == "play": #play response
 
             try:
 
@@ -100,10 +104,10 @@ class Client: #We will first connect the client to the server to subscribe himse
 
         try:
 
-            self.s.send(json.dumps(message).encode())
+            self.client.sendall(json.dumps(message).encode())
 
         except Exception as e:
-
+        
             print(f'Message not sent : {e}')
             
 
@@ -113,7 +117,4 @@ message = {"request": "subscribe",
            "name": "Redbull",
            "matricules": ["23363", "23049"]
            }
-client = Client('172.17.10.133', 3000, message)
-
-
-        
+client = Client('192.168.184.107', 3000, message)
