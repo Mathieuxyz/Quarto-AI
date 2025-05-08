e88323d4fc42dfea4b3e04039403209a38c0f658
Mastrogiovanni Amedeo <23046@ecam.be>
2025-05-08 09:14:36 +0200

    ok

--- pooling.py
+++ pooling.py
@@ -81,7 +81,7 @@ def win(board: dict, lines: list):
     return False 
 
 def chose_case():
-    free = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15}
+    free = {0, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13 , 14, 15}
     test_board = board
     for i in test_board:
         free = free - {i}
