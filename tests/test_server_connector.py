5e46ec1968ff54e99e798f47d2f267302d495f5f
Mathieuxyz <franchimont.mathieu@gmail.com>
2025-05-08 21:21:12 +0200

    all tests on server_connector are working

--- tests/test_server_connector.py
+++ tests/test_server_connector.py
@@ -1,19 +1,27 @@
 import Socket.server_connector as connector
 
-def test_initalization():
+from unittest.mock import MagicMock
 
-    port = '127.0.0.0'
-    host = 3333
+def test_initalization(): #working fine
+
+    host = '127.0.0.0'
+    port = 3333
     message = 'message de test'
-    client = connector.Client(port, host, message)
+    client = connector.Client(host, port, message)
 
-    assert client.port == port
     assert client.host == host
+    assert client.port == port
     assert client.message == message
 
-def test_response_treatment_ping():
+def test_response_treatment_ping(): #working fine
 
-    client = connector.Client()
+    host = '127.0.0.0'
+    port = 3333
+    message = 'message de test'
+    client = connector.Client(host, port, message)
+    
+    client.message_sender = MagicMock()
+
     request = {"request": "ping"}
     client.response_treatment(request)
 
@@ -21,10 +29,16 @@ def test_response_treatment_ping():
 
 
 def test_response_treatment_play():
-    client = connector.Client()
-    request = {"request": "play"}
+    host = '127.0.0.0'
+    port = 3333
+    message = 'message de test'
+    client = connector.Client(host, port, message)
+
+    client.message_sender = MagicMock()
+
+    request = {"request": "play", "state": {"players": ["LUR", "FKY"],"current": 0,"board": [null,"BDEC",null,"SDFP",null,null,null,null,null,"SLFC",null,null,"BLFP","BLEC",null,null], "piece": "BLEP"}}
     client.response_treatment(request)
 
-    client.message_sender.assert_called_once_with({"response": "move", "move": set(), "message": "La calotte de tes morts"})
-
+    client.message_sender.assert_called_once_with({"response": "move", "move": any, "message": "La calotte de tes morts"})
+    assert isinstance(args[0]['move'], dict)
 
